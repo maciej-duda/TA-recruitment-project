1611fe06eac5eba3550ed94ef338d3f80f8ca079
Maciej Duda <micioru@gmail.com>
2021-05-31 19:44:06 +0200

    formularze

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//npm install 
+//npm install
 //Jak by sie cos jebalo
 //npm install nodemon -g
 //npm install --save-dev cross-env
@@ -6,19 +6,19 @@
 //Zaczynac serwer w developmencie przez komende npm run dev
 //Zaczynac serwer w produkcji przez komende npm run production
 
-const express = require('express')
-const path = require('path')
-const connectDB = require('./config/db')
-const logger = require('morgan')
-const passport = require('passport')
-const flash = require('connect-flash')
-const session = require('express-session')
+const express = require("express");
+const path = require("path");
+const connectDB = require("./config/db");
+const logger = require("morgan");
+const passport = require("passport");
+const flash = require("connect-flash");
+const session = require("express-session");
 
 //Load env file
-require('dotenv').config({path: "./config/.env"})
+require("dotenv").config({ path: "./config/.env" });
 
 //Connect to db
-connectDB()
+connectDB();
 
 //Passport config
 //require('./config/passport')(passport)
@@ -27,37 +27,41 @@ connectDB()
 var app = express();
 
 //Logger middleware
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 
 // bodyparser middleware
-app.use(express.urlencoded({ extended: false}))
+app.use(express.urlencoded({ extended: false }));
 
 //Session configuration
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: false
-}))
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 //Flash errors middleware
 app.use(flash());
 
 //View engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 //Passport middleware
 //app.use(passport.initialize())
 //app.use(passport.session())
 
 //Static files middleware
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 //Routes
-var indexRouter = require('./routes/index');
-app.use('/', indexRouter);
+var indexRouter = require("./routes/index");
+app.use("/", indexRouter);
 
-app.listen(process.env.PORT, function() {
-  console.log(`App is running in ${process.env.Node_ENV} mode on port ${process.env.PORT}`)
-})
+app.listen(process.env.PORT, function () {
+  console.log(
+    `App is running in ${process.env.Node_ENV} mode on port ${process.env.PORT}`
+  );
+});
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,20 @@
-var express = require('express')
-var router = express.Router()
-var passport = require('passport')
+var express = require("express");
+var router = express.Router();
+var passport = require("passport");
 
 //var register = require('../functions/register.js')
 
 //GET
-router.get('/', async function(req, res) {
-    res.render('index')
+router.get("/", async function (req, res) {
+  res.render("index");
+});
+
+router.get("/login", async function (req, res) {
+  res.render("login");
+});
+
+router.get("/register", async function (req, res) {
+  res.render("register");
 });
 
 //POST
