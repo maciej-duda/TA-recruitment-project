6cf2ac1576b215a63d76a306658cf90ff766b365
Maciej Duda <micioru@gmail.com>
2021-06-03 17:56:52 +0200

    profile + very basic front

--- routes/index.js
+++ routes/index.js
@@ -4,22 +4,22 @@ var passport = require("passport");
 //const homeController = require("../controllers/home");
 const uploadController = require("../controllers/upload");
 
-var register = require('../functions/register.js');
+var register = require("../functions/register.js");
 
 //GET
 router.get("/", async function (req, res) {
-  var errors = req.flash().error || []
-  console.log(errors)
-  if(req.user){
-    console.log(req.user)
-    res.render('index', {
+  var errors = req.flash().error || [];
+  console.log(errors);
+  if (req.user) {
+    console.log(req.user);
+    res.render("index", {
       user: req.user,
-      errors: errors
-    })
-  }else{
-    res.render('index', {
-      errors: errors
-    })
+      errors: errors,
+    });
+  } else {
+    res.render("index", {
+      errors: errors,
+    });
   }
 });
 
@@ -31,26 +31,27 @@ router.get("/register", async function (req, res) {
   res.render("register");
 });
 
-router.get("/upload", async function (req, res){
-  res.render('upload');
+router.get("/upload", async function (req, res) {
+  res.render("upload");
+});
+
+router.get("/profile", async function (req, res) {
+  res.render("profile");
 });
 //POST
 
-router.post('/register', async function(req, res) {
-    register(req, res)
- });
+router.post("/register", async function (req, res) {
+  register(req, res);
+});
 
- router.post('/login', (req, res, next) => {
-  passport.authenticate('local', {
-      successRedirect: '/',
-      failureRedirect: '/login',
-      failureFlash: true
-  })(req, res, next)
-})
- 
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })(req, res, next);
+});
 
+router.post("/upload", uploadController.uploadFile);
 
-  router.post("/upload", uploadController.uploadFile);
-
-
-module.exports = router
+module.exports = router;
